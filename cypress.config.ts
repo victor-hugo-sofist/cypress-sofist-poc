531d992d0b21ccecda8f994215c9c424900b83ef
Victor Hugo <victor.silva@sofist.com.br>
2023-07-18 15:51:43 -0300

    chromeweb security

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    chromeWebSecurity:false,
     env: {
       saucedemo_url: "https://www.saucedemo.com/",
       username_standard_user: "standard_user",
