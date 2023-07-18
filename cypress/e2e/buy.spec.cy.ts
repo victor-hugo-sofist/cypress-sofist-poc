1ccdb98867cbf4da5ea4081d5e7f4900a1dbc801
Victor Hugo <victor.silva@sofist.com.br>
2023-07-18 12:08:26 -0300

    wip

--- cypress/e2e/buy.spec.cy.ts
+++ cypress/e2e/buy.spec.cy.ts
@@ -14,11 +14,6 @@ import { randomZipCode } from "../support/utils/zip-code";
 
 const url: string = Cypress.env("saucedemo_url");
 
-const products = productName;
-const cust = customer;
-const pageTitle = title;
-const pageMessage = message;
-
 describe("Module buy:", () => {
   const login: loginPage = new loginPage(url);
   const inventory: inventoryPage = new inventoryPage(url);
@@ -29,47 +24,47 @@ describe("Module buy:", () => {
 
   beforeEach(function () {
     login.access();
-  });
-
-  it("standard user should buy a product", () => {
-    let productIndex: number = Math.round(Math.random() * 4);
 
     login.withUser(
       Cypress.env("username_standard_user"),
       Cypress.env("password"),
     );
+  });
+
+  it("standard user should buy a product", () => {
+    let productIndex: number = Math.round(Math.random() * 4);
 
     cy.url().should("be.equal", url + "inventory.html");
     inventory.isVisible();
-    cy.contains(pageTitle.inventory).should("be.visible");
+    cy.contains(title.inventory).should("be.visible");
     inventory.selectARandomProduct(productIndex);
     inventory.clickInCart();
 
     cy.url().should("be.equal", url + "cart.html");
     cart.isVisible();
-    cy.contains(pageTitle.cart).should("be.visible");
-    cy.contains(products[productIndex]).should("be.visible");
+    cy.contains(title.cart).should("be.visible");
+    cy.contains(productName[productIndex]).should("be.visible");
     cart.clickInCheckout();
 
     cy.url().should("be.equal", url + "checkout-step-one.html");
     checkoutOne.isVisible();
-    cy.contains(pageTitle.checkoutOne).should("be.visible");
-    checkoutOne.fillFirstName(cust.firstName);
-    checkoutOne.fillLastName(cust.lastName);
+    cy.contains(title.checkoutOne).should("be.visible");
+    checkoutOne.fillFirstName(customer.firstName);
+    checkoutOne.fillLastName(customer.lastName);
     checkoutOne.fillZipCode(randomZipCode());
     checkoutOne.clickInContinue();
 
     cy.url().should("be.equal", url + "checkout-step-two.html");
     checkoutTwo.isVisible();
-    cy.contains(pageTitle.checkoutTwo).should("be.visible");
-    cy.contains(products[productIndex]).should("be.visible");
+    cy.contains(title.checkoutTwo).should("be.visible");
+    cy.contains(productName[productIndex]).should("be.visible");
     checkoutTwo.clickInFinish();
 
     cy.url().should("be.equal", url + "checkout-complete.html");
     checkoutComplete.isVisible();
-    cy.contains(pageTitle.checkoutComplete).should("be.visible");
-    cy.contains(pageMessage.thanksForOrder).should("be.visible");
-    cy.contains(pageMessage.orderDispatched).should("be.visible");
+    cy.contains(title.checkoutComplete).should("be.visible");
+    cy.contains(message.thanksForOrder).should("be.visible");
+    cy.contains(message.orderDispatched).should("be.visible");
     checkoutComplete.clickInBackHome();
     cy.url().should("be.equal", url + "inventory.html");
   });
