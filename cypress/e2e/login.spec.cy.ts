0ca018044c4bbfe764b8cd06eaca82370f089f2d
Victor Hugo <victor.silva@sofist.com.br>
2023-07-18 16:11:38 -0300

    teste

--- cypress/e2e/login.spec.cy.ts
+++ cypress/e2e/login.spec.cy.ts
@@ -1,7 +1,8 @@
 import { loginPage } from "../support/page/login";
 import { inventoryPage } from "../support/page/inventory";
 
-const url: string = Cypress.env("saucedemo_url");
+// const url: string = Cypress.env("saucedemo_url");
+const url: string = 'https://www.saucedemo.com/';
 
 describe("Module login:", () => {
   const login: loginPage = new loginPage(url);
