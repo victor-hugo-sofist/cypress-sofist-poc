4437925dfcd6ccef90a6379791884491b63e616e
Victor Hugo <victor.silva@sofist.com.br>
2023-07-19 15:44:01 -0300

    feat: Allure report with cypress 12.14.0

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "cypress";
 
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
     chromeWebSecurity: false, //This config it is necessary to run tests with Edge and Electron.
     env: {
